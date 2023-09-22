dd818ae1b9536c17097f47364b3fedb011db45e6
ivan-tapiam <ivan.tapiam@mayor.cl>
2023-09-21 21:03:28 -0300

    Primer Commit en una rama

--- Desafios_JavaScript/D1_calculando_total/assets/JS/script.js
+++ Desafios_JavaScript/D1_calculando_total/assets/JS/script.js
@@ -1,23 +1,3 @@
-const precio=100;
-let message= `El precio es: ${precio}`; //Para concatenar se deben usar los backsticks//
-document.querySelector("#precio").innerHTML=message;
+const precio= 400000;
 
-//Funcion flecha que permite tomar los valores desde HTML con el valor value//
-const onCompra=(formulario)=>{
-    let cantidad=formulario.cantidad.value
-    let color=formulario.color.value
-    let total = cantidad * precio
-    document.querySelector(".valorTotal").innerHTML= `El total de la compra es de ${total}`
-    
-    if(cantidad == `` || color == ``){
-        alert("debe ingresar una cantidad")
-    }
-
-    if(color == "rojo"){
-        mensaje="Elegiste rojo"
-        document.querySelector(".colorElegido").innerHTML=mensaje
-        document.querySelector(".colorElegido").style.color= "red"
-    }
-    
-    return false
-}
+precio= document.querySelector(".card-text").innerHTML='Precio: '+ precio
