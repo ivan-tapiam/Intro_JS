ac9f026145ec3f2c3926359d53dc9003bc96d67b
ivan-tapiam <ivan.tapiam@mayor.cl>
2023-12-08 20:04:10 -0300

    Cambios realizados

--- Desafios_JavaScript/D1_calculando_total/assets/JS/script.js
+++ Desafios_JavaScript/D1_calculando_total/assets/JS/script.js
@@ -1,3 +1,14 @@
 const precio= 400000;
+document.querySelector(".card-text").innerHTML='Precio: '+ precio
 
-precio= document.querySelector(".card-text").innerHTML='Precio: '+ precio
+function compra(){
+    const botonCantidad = document.getElementById('cantidad')
+    const valueBotonCantidad = botonCantidad.value;
+    if (valueBotonCantidad > 0) {
+        debugger
+    }
+    debugger
+    let cantidad = document.getElementById("cantidad").value;
+    let textColor = document.getElementById("text-color").value;
+    document.getElementById('total').value = cantidad * precio;
+}
